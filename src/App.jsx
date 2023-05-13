ab995a2932a6a9279bfd5dd1e925e489a3ae503f
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-13 13:17:35 +0600

    fix reg

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import Layout from "./Layout";
-import { Route , Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import QuestPage from "./page/QuestionPage";
 import AnswersPage from "./page/AnswersPage";
+// import { useState } from "react";
 function App() {
+  // const [isAuth, setIsAuth] = useState()
+
   return (
     <Layout>
-        <Routes>
-        <Route  path="/" element={<QuestPage />} />
-        <Route  path="/answers" element={<AnswersPage /> } />
+      <Routes>
+        <Route path="/" element={<QuestPage />} />
+        <Route path="/answers" element={<AnswersPage />} />
         <Route path="*" element={<h1>404 Не найдена страница</h1>} />
-        </Routes>
+      </Routes>
     </Layout>
   );
 }
