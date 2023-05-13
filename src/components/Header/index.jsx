2bdeb0ca496543df9fb09d05241b6be87f9eddd9
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-13 12:19:04 +0600

    add Header check  add bootsrap react add routers

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,20 @@
+import styles from "./style.module.css";
+import Button from 'react-bootstrap/Button';
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={styles.topMenu}>
+        <a className={styles.topMenuLink} href="/">
+          Вопросы 
+        </a>
+        <a className={styles.topMenuLink} href="/answers">
+          Ответы
+        </a>
+      </div>
+
+      <div className={styles.blockTopAuth}>
+      <Button variant="warning">Войти Админку</Button>{' '}
+      </div>
+    </header>
+  );
 };
